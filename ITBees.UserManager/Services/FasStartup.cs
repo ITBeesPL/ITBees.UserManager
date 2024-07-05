83098754fbe92bc4a6698baf1e0d770233b1b7a2
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-07-05 09:44:53 +0200

    Enable client ip address resolver

---
+++ ITBees.UserManager/Services/FasStartup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace ITBees.UserManager.Services;
+
+public class FasStartup
+{
+    /// <summary>
+    /// If You want to track ip addresses from incoming requests You need to register web application with XForwardedFor marks. Run this method inside program.cs 
+    /// </summary>
+    /// <param name="app"></param>
+    public static void EnableClientIpResolver(WebApplication app)
+    {
+        var forwardOptions = new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+        };
+
+        forwardOptions.KnownNetworks.Clear();
+        forwardOptions.KnownProxies.Clear();
+
+        app.UseForwardedHeaders(forwardOptions);
+    }
+}
