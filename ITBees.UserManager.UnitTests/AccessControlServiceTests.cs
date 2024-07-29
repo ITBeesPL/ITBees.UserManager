982a4374f431732909cf26e40112692a00dc0ab6
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-07-29 16:23:41 +0200

    Update unit tests

--- ITBees.UserManager.UnitTests/AccessControlServiceTests.cs
+++ ITBees.UserManager.UnitTests/AccessControlServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ITBees.Models.Companies;
 using ITBees.Models.Users;
 using ITBees.UserManager.Services.Acl;
 using ITBees.UserManager.Services.Registration;
@@ -15,10 +16,10 @@ namespace ITBees.UserManager.UnitTests
         {
             var user = new CurrentUser() {Guid= Guid.NewGuid()};
             var accessControl = new AccessControlService();
-            accessControl.Setup(new List<Type>(){typeof(NewUserRegistrationService<>)});
+            accessControl.Setup(new List<Type>(){typeof(NewUserRegistrationService<IdentityUser,Company>)});
             Guid companyGuid = new Guid();
             
-            var accessControlResult = accessControl.CanDo(user, typeof(NewUserRegistrationService<>), nameof(NewUserRegistrationService<IdentityUser>.CreateAndInviteNewUserToCompany), companyGuid);
+            var accessControlResult = accessControl.CanDo(user, typeof(NewUserRegistrationService<IdentityUser, Company>), nameof(NewUserRegistrationService<IdentityUser, Company>.CreateAndInviteNewUserToCompany), companyGuid);
 
             Assert.That(accessControlResult.CanDoResult);
         }
