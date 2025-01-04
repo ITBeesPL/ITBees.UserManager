77a65e40a6148b73a560f50c33eaf55975e9eff3
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-01-04 10:15:18 +0100

    Account delete using email

--- ITBees.UserManager/Controllers/AccountDeleteController.cs
+++ ITBees.UserManager/Controllers/AccountDeleteController.cs
@@ -21,14 +21,14 @@ public class AccountDeleteController<T> : RestfulControllerBase<AccountDeleteCon
 
 
     /// <summary>
-    /// Use this endpoint to delete own account, or specify guid of another user for delete.
+    /// Use this endpoint to delete own account, or specify guid or email (use null for guid if want to use email field) of another user for delete.
     /// </summary>
     /// <param name="accountGuid">If provided, then account of other user will be deleted</param>
     /// <returns></returns>
     [HttpDelete]
     [Produces<DeleteResultVm>]
-    public async Task<IActionResult> Del(Guid? accountGuid = null, string authKey = null)
+    public async Task<IActionResult> Del(Guid? accountGuid = null, string authKey = null, string email = null)
     {
-        return await ReturnOkResultAsync(async () => await _accountDeleteService.Delete(accountGuid, authKey));
+        return await ReturnOkResultAsync(async () => await _accountDeleteService.Delete(accountGuid, authKey, email));
     }
 }
--- ITBees.UserManager/Interfaces/IAccountDeleteService.cs
+++ ITBees.UserManager/Interfaces/IAccountDeleteService.cs
@@ -6,5 +6,5 @@ namespace ITBees.UserManager.Interfaces;
 
 public interface IAccountDeleteService<T> where T : IdentityUser<Guid>
 {
-    Task Delete(Guid? accountGuid, string authKey = null);
+    Task Delete(Guid? accountGuid, string authKey = null, string email = null);
 }
