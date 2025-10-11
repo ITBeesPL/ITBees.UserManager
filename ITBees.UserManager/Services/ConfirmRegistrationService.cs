c9b21730ba672f781edb2160896427d3002efc78
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-10-11 19:24:27 +0200

    Add logging to ConfirmRegistrationService for user confirmation errors

--- ITBees.UserManager/Services/ConfirmRegistrationService.cs
+++ ITBees.UserManager/Services/ConfirmRegistrationService.cs
@@ -8,6 +8,7 @@ using ITBees.UserManager.Interfaces;
 using ITBees.UserManager.Interfaces.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
 
 namespace ITBees.UserManager.Services
 {
@@ -15,11 +16,14 @@ namespace ITBees.UserManager.Services
     {
         private readonly IUserManager<T> _userManager;
         private readonly ILoginService<T> _loginService;
+        private readonly ILogger<ConfirmRegistrationService<T>> _logger;
 
-        public ConfirmRegistrationService(IUserManager<T> userManager, ILoginService<T> loginService)
+        public ConfirmRegistrationService(IUserManager<T> userManager, ILoginService<T> loginService,
+            ILogger<ConfirmRegistrationService<T>> logger)
         {
             _userManager = userManager;
             _loginService = loginService;
+            _logger = logger;
         }
 
         public async Task<TokenVm> ConfirmRegistrationEmailAndGetSessionToken(
@@ -28,6 +32,7 @@ namespace ITBees.UserManager.Services
             var user = await _userManager.FindByEmailAsync(confirmRegistrationIm.Email);
             if (user == null)
             {
+                _logger.LogError($"User {confirmRegistrationIm.Email} not found");
                 throw new Exception(
                     Translate.Get(() => Translations.UserManager.UserLogin.EmailNotRegistered, new En()));
             }
@@ -42,10 +47,13 @@ namespace ITBees.UserManager.Services
             }
 
             var errors = string.Join(";", confirmResult.Errors.Select(x => x.Description));
+            
+            _logger.LogError("Error on email confirmation for user {email} with token {token}. Errors: {errors}",
+                confirmRegistrationIm.Email, confirmRegistrationIm.Token, errors);
+            
             throw new Exception(Translate.Get(() => Translations.UserManager.UserLogin.ErrorOnConfirmationEmailAddress,
-                                    new En() +
-                                    $"Email :{confirmRegistrationIm.Email} token : {confirmRegistrationIm.Token} ") +
-                                "(" +
+                                    new En()) +
+                                $"Email :{confirmRegistrationIm.Email} token : {confirmRegistrationIm.Token} " + "(" +
                                 errors + ")");
         }
     }
