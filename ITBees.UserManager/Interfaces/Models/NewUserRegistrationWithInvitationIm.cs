cc885ad28ef3113649d790df564d281b49ac8008
Kacper <fastrabbiton@gmail.com>
2026-09-17 15:54:02 +0200

    Add Nip property to user account models and update related services

--- ITBees.UserManager/Interfaces/Models/NewUserRegistrationWithInvitationIm.cs
+++ ITBees.UserManager/Interfaces/Models/NewUserRegistrationWithInvitationIm.cs
@@ -17,6 +17,8 @@ namespace ITBees.UserManager.Interfaces.Models
 
         public string Phone { get; set; }
 
+        public string Nip { get; set; }
+
         public string Language { get; set; }
 
         public Guid? UserRoleGuid { get; set; }
